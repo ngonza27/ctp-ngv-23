e9bbe9d28ce8e871b11f217d8bfd8b3754dd2072
ngonzaa27 <ngonzaa27@gmail.com>
2023-06-01 11:41:00 +0100

    Adding notes for current work

--- src/script.py
+++ src/script.py
@@ -1,4 +1,17 @@
 import sys
 
+
+def get_dtc():
+  print("Data received")
+  return 0
+
+
+def send_data():
+  print("Sending data")
+  return 0
+
+
 if __name__ == "__main__":
+  get_dtc()
+  send_data()
   sys.exit()
