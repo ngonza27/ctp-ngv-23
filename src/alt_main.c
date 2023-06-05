8ccaf792a306c040c6bf427c2c44a6820cda1caf
ngonzaa27 <ngonzaa27@gmail.com>
2023-06-05 11:01:09 +0100

    Adding code to get Vehicle speed + alt main

--- src/alt_main.c
+++ src/alt_main.c
@@ -5,40 +5,35 @@
 #include <linux/can.h>
 #include <linux/can/raw.h>
 
-int
-main() {
-  int s;
-  struct ifreq ifr;
-  struct sockaddr_can addr;
-  struct can_frame frame;
-
-  
-  if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
-    perror("Socket");
-    return 1;
-  }
-  strcpy(ifr.ifr_name, "vcan0" );
-  ioctl(s, SIOCGIFINDEX, &ifr);
-
-  addr.can_family = AF_CAN;
-  addr.can_ifindex = ifr.ifr_ifindex;
-
-  if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-    perror("Bind");
+int main() {
+  // Open the CAN interface.
+  int fd = open("/dev/can0", O_RDWR);
+  if (fd < 0) {
+    perror("open");
     return 1;
   }
 
+  // Set the bitrate.
+  struct can_config cfg = {
+    .bitrate = 500000,
+  };
+  if (ioctl(fd, CAN_SET_BITRATE, &cfg) < 0) {
+    perror("ioctl");
+    return 1;
+  }
+
+  // Send the OBD PID request.
   struct can_frame frame = {
     .can_id = 0x7DF,
     .can_dlc = 8,
     .data = {0x02, 0x01, 0x0C, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC},
   };
-  
-  if (write(s, &frame, sizeof(frame)) < 0) {
-    perror("Write");
+  if (write(fd, &frame, sizeof(frame)) < 0) {
+    perror("write");
     return 1;
   }
 
+  // Receive the response message.
   struct can_frame response;
   int nread = read(fd, &response, sizeof(response));
   if (nread < 0) {
@@ -46,14 +41,17 @@ main() {
     return 1;
   }
 
+  // Check if the response message is from the expected sender.
   if (response.can_id != 0x7E8) {
     fprintf(stderr, "Unexpected response message from ID 0x%02X\n", response.can_id);
     return 1;
   }
 
+  // Print the vehicle speed.
   printf("Vehicle speed: %d km/h\n", response.data[0]);
 
-  close(s);
+  // Close the CAN interface.
+  close(fd);
 
   return 0;
 }
