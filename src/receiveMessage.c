7e3e40f9664fb019750566b15cb515148c4b8b83
ngonza27 <ngonzaa27@gmail.com>
2023-06-19 16:12:13 +0100

    DTC Decoding working

--- src/receiveMessage.c
+++ src/receiveMessage.c
@@ -10,11 +10,12 @@
 #define SERVICE_2 0x42
 #define SERVICE_3 0x43
 #define SERVICE_7 0x47
+#define DTC_TO_PRINT 10
 
 // int service_one(__u8 data[], int length);
-//unsigned int * service_two(int s, __u8 data[], int length);
-unsigned int * service_three(int s, __u8 data[], int length);
-//unsigned int * service_seven(int s, __u8 data[], int length);
+//unsigned int *service_two(int s, __u8 data[], int length);
+char (*service_three(int s, __u8 data[], int length))[6];
+//unsigned int *service_seven(int s, __u8 data[], int length);
 
 int receive_obd_message(int s) {
   int nbytes;
@@ -28,11 +29,10 @@ int receive_obd_message(int s) {
 		return EXIT_FAILURE;
 	}
 
-	int service = frame.data[0];
+	int service = frame.data[1];
 	int length = frame.can_dlc;
 
 	if (frame.can_id == CAN_ID_R && length > 0) {
-		printf("entro al switch case con servicio %d", service);
 		switch (service) {
 			case SERVICE_1: // Service 01
 				// int mil_is_on = service_one(frame.data, length);
@@ -42,26 +42,27 @@ int receive_obd_message(int s) {
 				// }
 				break;
 			case SERVICE_2: // Service 02
-				printf("Entro al service02");
-				unsigned int *detected_s2 = service_three(s, frame.data, length);
-				for (int i=0; i < 10; ++i){
-					printf("DTC LIST: %02X\n", detected_s2[i]);
-				}
+				// printf("Entro al service02\n");
+				// char *detected_s2 = (*service_three(s, frame.data, length))[5];
+				// for (int i=0; i < 10; ++i){
+				// 	printf("DTC LIST: %s\n", detected_s2[i]);
+				// }
 				break;
 			case SERVICE_3: // Service 03
-				unsigned int *detected_s3 = service_three(s, frame.data, length);
-				for (int i=0; i < 10; ++i){
-					printf("DTC LIST: %02X\n", detected_s3[i]);
+			printf("Entro al service03\n");
+				char (*detected_s3)[6] = service_three(s, frame.data, length);
+				for (int i=0; i < DTC_TO_PRINT; ++i){
+					printf("DTC LIST: %s\n", detected_s3[i]);
 				}
 				break;
 			case SERVICE_7: // Service 07
-				unsigned int *detected_s7 = service_three(s, frame.data, length);
-				for (int i=0; i < 10; ++i){
-					printf("DTC LIST: %02X\n", detected_s7[i]);
-				}
+			// printf("Entro al service07\n");
+			// 	char *detected_s7 = (*service_three(s, frame.data, length))[5];
+			// 	for (int i=0; i < 10; ++i){
+			// 		printf("DTC LIST: %s\n", detected_s7[i]);
+			// 	}
 				break;
 			default:
-				printf("Service not supported");
 				break;
 		}
 	}	else {
--- src/service03.c
+++ src/service03.c
@@ -5,6 +5,7 @@
 #include <sys/socket.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <string.h>
 
 #define MAX_DTC_PER_FRAME 3
 #define CAN_ID_R 0x7E8
@@ -12,24 +13,47 @@
 #define SERVICE_3 0x43
 #define SERVICE_7 0x47
 
-unsigned int detected_DTC[10];
+char detected_DTC[10][6];
+
+char *decode_dtc(unsigned int *dtc) {
+	unsigned int category =  (*dtc >> 14) & 0x03;
+	unsigned int is_sae_defined = (*dtc >> 12) & 0x03;
+	unsigned int number = *dtc & 0xFFF;
+	char* dtc_char = (char*)malloc(5 * sizeof(char));
+	switch (category) {
+		case 0:
+			sprintf(dtc_char, "P%d%3X ", is_sae_defined,number);
+			break;
+		case 1:
+			sprintf(dtc_char, "C%d%3X ", is_sae_defined,number);
+			break;
+		case 2:
+			sprintf(dtc_char, "B%d%3X ", is_sae_defined,number);
+			break;
+		case 3:
+			sprintf(dtc_char, "U%d%3X ", is_sae_defined,number);
+			break;
+		default:
+			break;
+	}
+	return dtc_char;
+}
 
 void extract_DTC(__u8 *data, int *counter, bool *keep_reading, int *total_dtc) {
 	int size = (*total_dtc < MAX_DTC_PER_FRAME) ? *total_dtc : MAX_DTC_PER_FRAME;
 	for (int i = 0; i < size; ++i) {
 		unsigned int dtc = (data[2+(i*2)]<<8) | data[3+(i*2)];
-		detected_DTC[(*counter)++] = dtc;
-		// printf("DTC: %04X\n", dtc);
+		char* decoded_dtc = decode_dtc(&dtc);
+		strncpy(detected_DTC[(*counter)++], decoded_dtc, sizeof(detected_DTC[i]) - 1);
+		free(decoded_dtc);
 	}
 	(*keep_reading) = ((*total_dtc)-MAX_DTC_PER_FRAME)>0;
 	(*total_dtc) -= MAX_DTC_PER_FRAME;
 }
 
-unsigned int *service_three(int s, __u8 *data, int length) {
-	// printf("CAN DLC: %02X\n", length);
-	// printf("RESPONSE SID: %02X\n", data[0]);
+char (*service_three(int s, __u8 *data, int length))[6] {
 	int counter = 0;
-	int total_dtc = data[1];
+	int total_dtc = data[0];
 	bool keep_reading = false;
 	
 	extract_DTC(data, &counter, &keep_reading, &total_dtc);
@@ -39,22 +63,23 @@ unsigned int *service_three(int s, __u8 *data, int length) {
 		if (recv(s, &frame, sizeof(struct can_frame), 0) == -1) {
 			close(s);
 			perror("Error receiving OBD-II CAN message\n");
-			return (unsigned int *)EXIT_FAILURE;
+			return (char (*)[6])EXIT_FAILURE;
 		}
 		// printf("CAN ID: %02X\n", frame.can_id);
 		// printf("CAN DLC: %02X\n", frame.can_dlc);
-		// printf("RESPONSE SID: %02X\n", frame.data[0]);
+		// printf("RESPONSE SID: %02X\n", frame.data[1]);
 		if (frame.can_id == CAN_ID_R && frame.can_dlc > 0) {
-			switch (frame.data[0]) {
+			switch (frame.data[1]) {
 				case SERVICE_2:
 				case SERVICE_3:
 				case SERVICE_7:
-					total_dtc = frame.data[1];  // Number of DTCs reported in this frame
+					total_dtc = frame.data[0];  // Number of DTCs reported in this frame
 					// printf("TOTAL DTC: %d\n", total_dtc);
 					extract_DTC(frame.data, &counter, &keep_reading, &total_dtc);
 					break;
 				default:
-					break;
+					printf("Service not supported");
+					break; 
 			}
 		}
 	}
