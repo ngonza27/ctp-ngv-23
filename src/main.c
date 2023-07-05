39f90f3fad7c3358d630839d95d037ed1daeb5ea
ngonza27 <ngonzaa27@gmail.com>
2023-07-05 14:35:50 +0100

    Rearranging thread creation + documenting main file

--- src/main.c
+++ src/main.c
@@ -31,6 +31,11 @@ struct send_struct {
 int send_obd_message(int s, int data[], int length);
 int receive_obd_message(int s, int service);
 
+
+/**
+ 	@brief Closes the socket before stoping the program
+	@param _ unused value
+*/
 static void sig_handler(int _) {
   (void)_;
   printf("\nHalting program\n");
@@ -41,6 +46,11 @@ static void sig_handler(int _) {
   exit(EXIT_SUCCESS);
 }
 
+
+/**
+ 	@brief Creates the connection with the CAN network
+	@return socket ID
+*/
 int setup_socket() {
   int s;
   struct sockaddr_can addr;
@@ -65,6 +75,12 @@ int setup_socket() {
   return s;
 }
 
+
+/**
+ 	@brief Creates thread to send request data
+	@param arg saves the values of the socket_id, length and data
+	@return NULL
+*/
 void* send_thread(void* arg) {
   struct send_struct* params = (struct send_struct*)arg;
   int* data = params->data;
@@ -75,29 +91,48 @@ void* send_thread(void* arg) {
   return NULL;
 }
 
+
+/**
+ 	@brief Creates thread to receive CAN data
+	@param arg saves the id of the service to which data will be received
+	@return NULL
+*/
 void* receive_thread(void* arg) {
   int* service = (int*)arg;
   mil_status = receive_obd_message(socket_id, *service);
   return NULL;
 }
 
+
+/**
+ 	@brief Creates thread to receive CAN data
+	@param data message information sent through CAN frames
+  @param socket_id id of the socket
+	@param service service to consult
+	@param send_t reference of send thread
+	@param receive_t reference of receive thread
+
+*/
 void execute_service(int *data, int socket_id, int service, pthread_t *send_t, pthread_t *receive_t) {
   struct send_struct params = {data, socket_id, MSG_LENGTH};
+  if (pthread_create(receive_t, NULL, receive_thread, &service) != 0) {
+    perror("Failed to create receive thread");
+    exit(EXIT_FAILURE);
+  }
 
-    if (pthread_create(send_t, NULL, send_thread, (void*)&params) != 0) {
-      perror("Failed to create send thread");
-      exit(EXIT_FAILURE);
-    }
-
-    if (pthread_create(receive_t, NULL, receive_thread, &service) != 0) {
-      perror("Failed to create receive thread");
-      exit(EXIT_FAILURE);
-    }
-    pthread_join(*send_t, NULL);
-    pthread_join(*receive_t, NULL);
+  if (pthread_create(send_t, NULL, send_thread, (void*)&params) != 0) {
+    perror("Failed to create send thread");
+    exit(EXIT_FAILURE);
+  }
+  
+  pthread_join(*send_t, NULL);
+  pthread_join(*receive_t, NULL);
 }
 
 // [#bytes, mode, PID, A, B, C, D]
+/**
+ 	@brief Entry point of the code as well as top level logic
+*/
 int main() {
   socket_id = setup_socket();
   signal(SIGINT, sig_handler);
@@ -107,7 +142,6 @@ int main() {
     receive_obd_message(socket_id, SERVICE_3);
   } else {
     while(1) {
-      // [#bytes, mode, PID, A, B, C, D]
       int data_s1[7] = {0x02, 0x01, 0x01, 0x55, 0x55, 0x55, 0x55};
       execute_service(data_s1, socket_id, SERVICE_1, &send_t, &receive_t);
       if(mil_status == 2) {
